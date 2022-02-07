f8e7731650870e1781274815ff57274e3c6a7652
Juan I. Carlis <juancarlis93@gmail.com>
2022-02-07 17:58:13 -0300

    creating views

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('Estás en la app de Premios')
+
+
+def detail(request, question_id):
+    return HttpResponse("Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse("Estás viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse("Estás votando la pregunta número {question_id}")
