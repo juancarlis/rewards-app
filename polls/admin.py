ae75989ba0f882124b947d013201e5ee39d67d9b
Juan I. Carlis <juancarlis93@gmail.com>
2022-02-08 20:21:43 -0300

    modify admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
